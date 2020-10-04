5600c98bdd01874ffc384c01d377ee40f8a453a4
Billbern <denis.abaidoo@gmail.com>
2020-10-04 23:52:39 +0000

    changed card style

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import CountUp from 'react-countup'
 
 function Card(prop) {
   return (
-    <div className="track_card" style={{borderBottom: `0.25rem solid ${prop.color}` }}>
+    <div className="track_card" style={{borderLeft: `0.35rem solid ${prop.color}` }}>
       <h5>{prop.title}</h5>
-      <p><CountUp start={0} end={prop.figure}/></p>
+      <p><CountUp start={0} end={prop.figure} delay={1} duration={2}/></p>
     </div>
   );
 }
