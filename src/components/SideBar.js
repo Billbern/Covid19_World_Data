05dd0bd0203e0879f7a73de1db99c7805042763b
Billbern <denis.abaidoo@gmail.com>
2020-10-05 06:59:18 +0000

    added date to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -11,16 +11,13 @@ function SideBar() {
                     <div className="meta_data">
                         <img src="" alt="" width="200" height="150"/>
                         <h3 className="country_name">Global</h3>
+                        <p><small>4th October, 2020</small></p>
                     </div>
                     <div className="count_data">
                         <Card title="Total Cases" figure="5000" color="#7ab5e2"/>
                         <Card title="Active Cases" figure="2000" color="#e3b934"/>
                         <Card title="Recovered Cases" figure="2500" color="#64cb53"/>
                         <Card title="Death Cases" figure="500" color="#d23e33"/>
-                    </div>
-                    <div className="legend">
-                        {/* <h4>Legend</h4> */}
-                        
                     </div>
                 </div>
             </div>
