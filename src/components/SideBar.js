f95b7e90d96c5b503a45145ceb0193c9725ebc82
Billbern <denis.abaidoo@gmail.com>
2020-10-06 04:09:09 +0000

    included date to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import Card from './Card';
-import CountrySelector from './CountrySelector';
-
+import React from "react";
+import Card from "./Card";
+import CountrySelector from "./CountrySelector";
 
 function SideBar() {
-    return(
-        <div className="right_side">
-                <CountrySelector />
-                <div className="country_data">
-                    <div className="meta_data">
-                        <img src="" alt="" width="200" height="150"/>
-                        <h3 className="country_name">Global</h3>
-                        <p><small>4th October, 2020</small></p>
-                    </div>
-                    <div className="count_data">
-                        <Card title="Total Cases" figure="5000" color="#7ab5e2"/>
-                        <Card title="Active Cases" figure="2000" color="#e3b934"/>
-                        <Card title="Recovered Cases" figure="2500" color="#64cb53"/>
-                        <Card title="Death Cases" figure="500" color="#d23e33"/>
-                    </div>
-                </div>
-            </div>
-    );
+  return (
+    <div className="right_side">
+      <CountrySelector />
+      <div className="country_data">
+        <div className="meta_data">
+          <img src="" alt="" width="200" height="150" />
+          <div className="country_name">
+            <h4>Global</h4>
+            <small>4th October, 2020</small>
+          </div>
+        </div>
+        <div className="count_data">
+          <Card title="Total Cases" figure="5000" color="#7ab5e2" />
+          <Card title="Active Cases" figure="2000" color="#e3b934" />
+          <Card title="Recovered Cases" figure="2500" color="#64cb53" />
+          <Card title="Death Cases" figure="500" color="#d23e33" />
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default SideBar;
